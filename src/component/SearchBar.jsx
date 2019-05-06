89ddb62272a0f94d8ab9fed52b610b6616eadeb7
rjustin <ryan.justin@ymail.com>
2019-05-06 23:59:59 +0800

    Get exercises from API and added debounce function

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -2,82 +2,84 @@ import React, {Component} from 'react';
 import Autosuggest from 'react-autosuggest';
 import AutosuggestHighlightMatch from "autosuggest-highlight/umd/match";
 import AutosuggestHighlightParse from "autosuggest-highlight/umd/parse";
+import axios from 'axios';
+import _ from 'lodash';
 import '../css/SearchBar.css'
 
-const exercises = [
-    {
-      _id: 1,
-      name: 'Bench Press',
-    },
-    {
-      _id: 2,
-      name: 'Shoulder Press',
-    },
-    {
-      _id: 3,
-      name: 'Lat Pulldown',
-    },
-    {
-      _id: 4,
-      name: 'Squat',
-    },
-    {
-      _id: 5,
-      name: 'Lunges',
-    },
-    {
-      _id: 6,
-      name: 'Barbel Row',
-    },
-    {
-      _id: 7,
-      name: 'Dumbell Row',
-    },
-    {
-      _id: 8,
-      name: 'Biceps Curl',
-    },
-    {
-      _id: 9,
-      name: 'Triceps Extension',
-    },
-    {
-      _id: 10,
-      name: 'One Arm Row',
-    },
-    {
-      _id: 11,
-      name: 'Pec Fly',
-    },
-    {
-      _id: 12,
-      name: 'Lateral Raise',
-    },
-    {
-      _id: 13,
-      name: 'Cable Row',
-    },
-    {
-      _id: 14,
-      name: 'Dumbell Curl',
-    }
-  ];
+// const exercises = [
+//     {
+//       _id: 1,
+//       name: 'Bench Press',
+//     },
+//     {
+//       _id: 2,
+//       name: 'Shoulder Press',
+//     },
+//     {
+//       _id: 3,
+//       name: 'Lat Pulldown',
+//     },
+//     {
+//       _id: 4,
+//       name: 'Squat',
+//     },
+//     {
+//       _id: 5,
+//       name: 'Lunges',
+//     },
+//     {
+//       _id: 6,
+//       name: 'Barbel Row',
+//     },
+//     {
+//       _id: 7,
+//       name: 'Dumbell Row',
+//     },
+//     {
+//       _id: 8,
+//       name: 'Biceps Curl',
+//     },
+//     {
+//       _id: 9,
+//       name: 'Triceps Extension',
+//     },
+//     {
+//       _id: 10,
+//       name: 'One Arm Row',
+//     },
+//     {
+//       _id: 11,
+//       name: 'Pec Fly',
+//     },
+//     {
+//       _id: 12,
+//       name: 'Lateral Raise',
+//     },
+//     {
+//       _id: 13,
+//       name: 'Cable Row',
+//     },
+//     {
+//       _id: 14,
+//       name: 'Dumbell Curl',
+//     }
+//   ];
 
 function escapeRegexCharacters(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
   
-function getSuggestions(value) {
-    const escapedValue = escapeRegexCharacters(value.trim());
+//function getSuggestions(value) {
+//     const escapedValue = escapeRegexCharacters(value.trim());
     
-    if (escapedValue === '') {
-        return [];
-    }
+//     if (escapedValue === '') {
+//         return [];
+//     }
   
-    const regex = new RegExp('^' + escapedValue, 'i');
+//     const regex = new RegExp('^' + escapedValue, 'i');
   
-    return exercises.filter(exercise => regex.test(exercise.name));
-}
+//     return exercises.filter(exercise => regex.test(exercise.name));
+//}
   
 function getSuggestionValue(suggestion) {
     return suggestion.name;
@@ -100,11 +102,29 @@ function renderSuggestion(suggestion, { query }) {
   }
 
 class SearchBar extends Component {
-    state = {
-        task: {},
-        value: '',
-        suggestions: [],
-        selected: ''
+    constructor() {
+      super();
+
+      this.state = {
+          task: {},
+          value: '',
+          suggestions: [],
+          selected: ''
+      }
+
+      this.debouncedLoadSuggestions = _.debounce(this.loadSuggestions, 500);
+    }
+
+    loadSuggestions(value) {
+      
+        axios.get(process.env.REACT_APP_GET_EXERCISE_URL + `?search=${value}`)
+          .then(response => {
+            console.log('response', response);
+            this.setState({ suggestions: response.data })
+          })
+          .catch(error => {
+            console.log('error', error);
+          });
     }
 
     onChange = (event, { newValue }) => {
@@ -112,7 +132,16 @@ class SearchBar extends Component {
     };
 
     onSuggestionsFetchRequested = ({ value }) => {
-        this.setState({suggestions: getSuggestions(value)});
+        this.debouncedLoadSuggestions(value);
+        //this.setState({suggestions: getSuggestions(value)});
+        // axios.get(process.env.REACT_APP_GET_EXERCISE_URL + `?search=${value}`)
+        //   .then(response => {
+        //     console.log('response', response);
+        //     this.setState({ suggestions: response.data })
+        //   })
+        //   .catch(error => {
+        //     console.log('error', error);
+        //   });
     };
 
     onSuggestionsClearRequested = () => {
