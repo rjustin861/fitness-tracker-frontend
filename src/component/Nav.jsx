2e591a229365a4102bdbc599bec3c56aac9d8dc6
Pezzini S <susanna.pezzini@gmail.com>
2019-05-09 16:36:19 +0700

    track all files

---
+++ src/component/Nav.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../css/Nav.css'
+
+class Nav extends Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default Nav;
