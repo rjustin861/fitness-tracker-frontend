e12458ad2b53c98b24aac1c852e8b1d3e8c4aaae
Pezzini S <susanna.pezzini@gmail.com>
2019-05-15 14:50:28 +0700

    add patch for users location

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -8,6 +8,8 @@ import WithAuth from '../service/WithAuth';
 import Header from './Header';
 import DashboardBody from './DashboardBody';
 import GeoHelperService from '../service/GeoHelperService';
+import axios from 'axios';
+import { userInfo } from 'os';
 
 class Dashboard extends Component {
   Auth = new AuthHelperService();
@@ -21,11 +23,28 @@ class Dashboard extends Component {
   componentWillMount() {
     this.Geo.getCoordinates()
       .then((response) => {
-          console.log('coordinates data', response); //{lat: 9.7165312, long: 99.98581759999999}
-      })
-      .catch((error) => {
+          console.log('coordinates data', response); //{lat: 9.7165312, long: 99.98581759999999
+          const tokenStr = this.Auth.getToken();
+          console.log({tokenStr})
+          let long = response.long
+          let lat = response.lat
+          let location = {
+            type: 'Point',
+            coordinates: [long, lat]
+          }
+          console.log({location})
+          axios.patch(process.env.REACT_APP_PATCH_USER, {location}, { headers: {"Authorization" : `Bearer ${tokenStr}`} } )
+          .then( (response) => {
+            	console.log({response})
+            	})
+            	.catch( (error) => {
+            		console.log(error);
+            	})
+          
+        })
+        .catch((error) => {
           console.log('error', error);
-      })
+        })
   }
 
   render() {
